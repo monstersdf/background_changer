0c3a8a56c325ecd1118a4c6ca5dca50c36723040
ashish__12 <ashish.suresh@finalyca.com>
2023-08-04 20:11:13 +0530

    background changer

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+let  colorButtons = document.getElementsByClassName('bg-changer')
+// alert(colorButtons);
+
+let btns = document.getElementById('ki')
+console.log(btns);
+
+function colorChanger(color){
+    // document.body.style.backgroundColor = color;
+   btns.style.backgroundColor = color;
+    // alert();
+}
